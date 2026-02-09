6b4c847e24f9f2d6d8f8b9c40ac5e2105df2c5a0
michael-2956 <work.michael.2956@gmail.com>
2026-02-09 20:20:40 +0200

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react()].filter(Boolean),
-  base: "/mykhailyna.github.io/",
+  base: "/mykhailyna-narrative-tracking/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
